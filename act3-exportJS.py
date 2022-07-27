7d8c0db94279c7559a7ea2379352db4590271286
DavidSoh[BR11] <mail@stdj.me>
2022-07-27 23:38:41 +0800

    add exportJS.py for html/js testing

---
+++ act3-exportJS.py
@@ -0,0 +1,12 @@
+import tomlkit
+title_dict = tomlkit.load(open("annotation.toml"))
+
+print("Export playlist.js for testing")
+header_list = ["name", "apple", "google", "spotify", "feed"]
+title_list = list()
+for value_dict in title_dict.values():
+    value_inner_str = ",\n".join(["\"{}\": \"{}\"".format(header_str,value_dict.get(header_str,"")) for header_str in header_list])
+    title_list.append(value_inner_str)
+outer_str = "var playlist = [\n{\n"+"\n},\n{\n".join(title_list)+"\n}\n];"
+with open("docs/playlist.js",'w') as target_handler:
+    target_handler.write(outer_str)
