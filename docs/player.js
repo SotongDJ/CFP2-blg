0bf1ae1c247ed5756e7221381a59b974ef36ebd1
DavidSoh[BR11] <mail@stdj.me>
2022-07-28 23:09:57 +0800

    fix next() and adjust style: - use fontawesome - use padding replace space span

--- docs/player.js
+++ docs/player.js
@@ -3,42 +3,27 @@ var playlist_dom = document.getElementById("playlist");
 function link(href,short,jump) {
  if (href == "") {
   var gp = document.createElement('span');
-  gp.innerText = short;
+  gp.className = short;
   return gp;
  } else {
   var gp = document.createElement('a');
   gp.href = href;
   if (jump) { gp.target = "podcast"; };
-  gp.innerText = short;
+  gp.className = short;
   return gp;
  };
 }
 for (let nub = 0; nub < playlist.length; nub++) {
 // ord = playlist.length - nub - 1;
  ord = nub;
- var li = document.createElement('li');
-
- li.appendChild(link(playlist[ord]["apple"],"A",true));
- var aps = document.createElement('span');
- aps.innerText = " ";
- li.appendChild(aps);
-
- li.appendChild(link(playlist[ord]["google"],"G",true));
- var ggs = document.createElement('span');
- ggs.innerText = " ";
- li.appendChild(ggs);
-
- li.appendChild(link(playlist[ord]["spotify"],"S",true));
- var sps = document.createElement('span');
- sps.innerText = " ";
- li.appendChild(sps);
- 
+ var li = document.createElement('p');
+ li.appendChild(link(playlist[ord]["apple"],"fa-brands fa-apple",true));
+ li.appendChild(link(playlist[ord]["google"],"fa-brands fa-google",true));
+ li.appendChild(link(playlist[ord]["spotify"],"fa-brands fa-spotify",true));
+ li.appendChild(link(playlist[ord]["feed"],"fa-solid fa-download",true));
  files.push(playlist[ord]['feed']);
- li.appendChild(link("javascript: void(goToPlay("+nub+"))","D",false));
- var fs = document.createElement('span');
- fs.innerText = " ";
- li.appendChild(fs);
- 
+ li.appendChild(link("javascript: void(goToPlay("+nub+"))","fa-solid fa-play",false));
+
  te = document.createElement("span");
  te.innerText = playlist[ord]['name'];
  li.appendChild(te);
@@ -49,6 +34,7 @@ var player_dom = document.getElementById("player");
 function next() {
  if (i === files.length - 1) { i = 0; } else { i++; };
  player_dom.src = files[i];
+ player_dom.play();
 };
 function goToPlay(ti) {
  player_dom.src = files[ti];
