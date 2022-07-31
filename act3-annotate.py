c874360370f0e89b38920a5bb8f9d47226bb5eb5
DavidSoh[BR11] <mail@stdj.me>
2022-07-31 23:53:34 +0800

    ignore single tag

--- act3-annotate.py
+++ act3-annotate.py
@@ -24,10 +24,11 @@ for entry_name, entry_detail in keyword_doc.items():
         inclusive_list, exclusive_list = check(inclusive_str,entry_detail['exclusive'])
         inclusive_collect_list.extend([n for n in inclusive_list if n not in inclusive_collect_list])
         exclusive_collect_list.extend([n for n in exclusive_list if n not in exclusive_collect_list])
-    print(F"{entry_name} found in: ({len(inclusive_collect_list)})")
-    print("  {}".format("\n  ".join(inclusive_collect_list)))
-    print(F"Excluded: ({len(exclusive_collect_list)})")
-    print("  {}\n".format("\n  ".join(exclusive_collect_list)))
+    if len(inclusive_collect_list) > 1:
+        print(F"{entry_name} found in: ({len(inclusive_collect_list)})")
+        print("  {}".format("\n  ".join(inclusive_collect_list)))
+        print(F"Excluded: ({len(exclusive_collect_list)})")
+        print("  {}\n".format("\n  ".join(exclusive_collect_list)))
     for episode_str in inclusive_collect_list:
         episode_table = structure_doc[episode_str]
         episode_tag_list = episode_table["tag"]
