8eb1a926373cd9c67930c688a5abce9bc88d9de3
DavidSoh[BR11] <mail@stdj.me>
2022-07-31 23:27:57 +0800

    output tag into playlist.js

--- act4-exportJS.py
+++ act4-exportJS.py
@@ -1,4 +1,4 @@
-import tomlkit
+import tomlkit, json
 title_dict = tomlkit.load(open("annotation.toml"))
 
 print("Export playlist.js for testing")
@@ -6,6 +6,7 @@ header_list = ["name", "apple", "google", "spotify", "feed"]
 title_list = list()
 for value_dict in title_dict.values():
     value_inner_str = ",\n".join(["\"{}\": \"{}\"".format(header_str,value_dict.get(header_str,"")) for header_str in header_list])
+    value_inner_str = value_inner_str + ",\n\"tag\": {}".format(json.dumps(value_dict.get("tag",list())))
     title_list.append(value_inner_str)
 outer_str = "var playlist = [\n{\n"+"\n},\n{\n".join(title_list)+"\n}\n];"
 with open("docs/playlist.js",'w') as target_handler:
