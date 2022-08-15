aec8322c8849ce07e24f8b58027865e357bd0c54
DavidSoh[BR11] <mail@stdj.me>
2022-08-16 01:29:43 +0800

    use direct output instead of JSON as the site not longer depend on parameter

--- act5-exportJS.py
+++ act5-exportJS.py
@@ -1,4 +1,4 @@
-import tomlkit, json
+import tomlkit
 title_dict = tomlkit.load(open("annotation.toml"))
 keyword_doc = tomlkit.load(open("keyword.toml"))
 
@@ -15,20 +15,26 @@ for enum_int, value_dict in enumerate(title_dict.values()):
     for tag in tag_list:
         if tag not in deduplicate_tag_list:
             deduplicate_tag_list.append(tag)
-    value_inner_str = value_inner_str + ",\n\"tag\": {}".format(json.dumps(deduplicate_tag_list))
+    value_inner_str = value_inner_str + ",\n\"tag\": {}".format(deduplicate_tag_list)
     title_list.append(value_inner_str)
 outer_str = "const playlist = {\n"+"\n},\n".join(title_list)+"\n}\n};\n"
-class_dict = dict()
+
+tag2class_dict = {tag_name:entry_detail["category"] for tag_name, entry_detail in keyword_doc.items()}
+tag2class_list = ["\"{}\": {}".format(tag_name,tag_category_list) for tag_name, tag_category_list in tag2class_dict.items()]
+tag2class_str = "const tag_class = {\n"+",\n".join(tag2class_list)+"\n};\n"
+
+class2tag_dict = dict()
 for tag_name, entry_detail in keyword_doc.items():
     for category_name in entry_detail["category"]:
-        category_list = class_dict.get(category_name,list())
+        category_list = class2tag_dict.get(category_name,list())
         category_list.append(tag_name)
-        class_dict[category_name] = category_list
-class_list = list()
-for category_name, category_list in class_dict.items():
-    class_str = "\",\"".join(category_list)
-    class_list.append("\"{}\": [\"{}\"]".format(category_name,class_str))
-category_str = "const tag_class = {\n"+",\n".join(class_list)+"\n};\n"
+        class2tag_dict[category_name] = category_list
+class2tag_list = list()
+for category_name, category_list in class2tag_dict.items():
+    class2tag_list.append("\"{}\": {}".format(category_name,category_list))
+class2tag_str = "const class_tag = {\n"+",\n".join(class2tag_list)+"\n};\n"
+
 with open("docs/playlist.js",'w') as target_handler:
     target_handler.write(outer_str)
-    target_handler.write(category_str)
+    target_handler.write(tag2class_str)
+    target_handler.write(class2tag_str)
