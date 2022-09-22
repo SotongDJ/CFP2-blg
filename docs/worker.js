65b09bf7f091ecefa7a3a5ee9fea686f27be62a1
David Soh <mail@stdj.me>
2022-09-22 18:00:36 +0800

    fix preload error disable preload

--- docs/worker.js
+++ docs/worker.js
@@ -1,24 +1,10 @@
 // reference:
 // - https://github.com/mdn/sw-test
+// - https://web.dev/navigation-preload/
 // - https://developer.chrome.com/docs/workbox/caching-strategies-overview/
+// - https://jakearchibald.com/2014/offline-cookbook/#stale-while-revalidate
 //
-const currentVersion = 'v1.4';
-
-const addResourcesToCache = async (resources) => {
-const cache = await caches.open(currentVersion);
-await cache.addAll(resources);
-};
-
-const putInCache = async (request, response) => {
-const cache = await caches.open(currentVersion);
-await cache.put(request, response);
-};
-  
-const enableNavigationPreload = async () => {
-if (self.registration.navigationPreload) {
-await self.registration.navigationPreload.enable();
-};
-};
+const currentVersion = 'v1.5';
 
 const deleteCache = async (key) => {await caches.delete(key);};
 
@@ -29,36 +15,48 @@ const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
 await Promise.all(cachesToDelete.map(deleteCache));
 };
 
+addEventListener('activate', event => {
+event.waitUntil(async function() {
+if (self.registration.navigationPreload) {
+await self.registration.navigationPreload.disable();
+}
+}());
+event.waitUntil(deleteOldCaches());
+});
+
+const addResourcesToCache = async (resources) => {
+const cache = await caches.open(currentVersion);
+await cache.addAll(resources);
+};
+
+const precache_arr = [
+'/font.css','/icon.css','/index.html',
+'/main.css','/main.js','/main.webmanifest',
+'/register.js'
+];
+
+self.addEventListener('install', (event) => {
+event.waitUntil(addResourcesToCache(precache_arr));
+});
+
 const responseHandle = async (event) => {
 const cachedResponse = await caches.open(currentVersion).then(async (cache) => {
 const cachedResp = await cache.match(event.request);
 return cachedResp;
 });
 const fetchedResponse = fetch(event.request).then(async (networkResponse) => {
-await putInCache(event.request, networkResponse.clone());
+const cache = await caches.open(currentVersion);
+await cache.put(event.request, networkResponse.clone());
 return networkResponse;
 });
-const preloadResponse = await event.preloadResponse;
-return cachedResponse || (fetchedResponse || preloadResponse);
+return cachedResponse || fetchedResponse;
 }
-
-precache_arr = [
-'/','/font.css','/icon.css','/index.html',
-'/main.css','/main.js','/main.webmanifest',
-'/playlist.js','/register.js',
-];
-
-self.addEventListener('activate', (event) => {
-event.waitUntil(enableNavigationPreload());
-event.waitUntil(deleteOldCaches());
-});
-
-self.addEventListener('install', (event) => {
-event.waitUntil(addResourcesToCache(precache_arr));
-});
 
 self.addEventListener('fetch', async (event) => {
 // if (event.request.url.match('^.*(\.mp3)$')) {return false;};
-if (event.request.url.match('^.*(www\.buzzsprout\.com)*.$')) {return false;};
+if (event.request.url.match('^.*(www\.buzzsprout\.com)*.$')) {
+await event.respondWith(fetch(event.request));
+} else {
 await event.respondWith(responseHandle(event));
+};
 });
