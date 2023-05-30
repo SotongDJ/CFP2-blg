36ce145dc908baf13be2dc8274f236fb96f381c0
David Soh <mail@stdj.me>
2023-05-30 08:26:05 +0000

    chore: add annotation from copilot

--- docs/register.js
+++ docs/register.js
@@ -1,17 +1,24 @@
+// Define an asynchronous function to register a service worker
 const registerServiceWorker = async () => {
- if ('serviceWorker' in navigator) {
-  try {
-   const registration = await navigator.serviceWorker.register('/worker.js',{scope: '/'});
-   if (registration.installing) {
-    console.log('Service worker installing');
-   } else if (registration.waiting) {
-    console.log('Service worker installed');
-   } else if (registration.active) {
-    console.log('Service worker active');
-   };
-  } catch (error) {
-   console.error(`Registration failed with ${error}`);
-  };
- };
+    // Check if the browser supports service workers
+    if ('serviceWorker' in navigator) {
+        try {
+            // Register the service worker with the specified file path and scope
+            const registration = await navigator.serviceWorker.register('/worker.js', { scope: '/' });
+            // Check the status of the service worker registration
+            if (registration.installing) {
+                console.log('Service worker installing');
+            } else if (registration.waiting) {
+                console.log('Service worker installed');
+            } else if (registration.active) {
+                console.log('Service worker active');
+            };
+        } catch (error) {
+            // Log an error message if the service worker registration fails
+            console.error(`Registration failed with ${error}`);
+        };
+    };
 };
+
+// Call the registerServiceWorker function to register the service worker
 registerServiceWorker();
--- docs/worker.js
+++ docs/worker.js
@@ -3,60 +3,86 @@
 // - https://web.dev/navigation-preload/
 // - https://developer.chrome.com/docs/workbox/caching-strategies-overview/
 // - https://jakearchibald.com/2014/offline-cookbook/#stale-while-revalidate
-//
+
+// Set the current version of the service worker
 const currentVersion = 'v0.2.1';
 
-const deleteCache = async (key) => {await caches.delete(key);};
-
-const deleteOldCaches = async () => {
-const cacheKeepList = [currentVersion];
-const keyList = await caches.keys();
-const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
-await Promise.all(cachesToDelete.map(deleteCache));
+// Function to delete a cache with a given key
+const deleteCache = async (key) => {
+    await caches.delete(key);
 };
 
+// Function to delete all caches except the current version
+const deleteOldCaches = async () => {
+    // List of caches to keep
+    const cacheKeepList = [currentVersion];
+    // Get a list of all cache keys
+    const keyList = await caches.keys();
+    // Filter out the caches to delete
+    const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+    // Delete the caches
+    await Promise.all(cachesToDelete.map(deleteCache));
+};
+
+// Event listener for when the service worker is activated
 addEventListener('activate', event => {
-event.waitUntil(async function() {
-if (self.registration.navigationPreload) {
-await self.registration.navigationPreload.disable();
-}
-}());
-event.waitUntil(deleteOldCaches());
+    event.waitUntil(async function() {
+        // Disable navigation preload if it is supported
+        if (self.registration.navigationPreload) {
+            await self.registration.navigationPreload.disable();
+        }
+    }());
+    // Delete old caches
+    event.waitUntil(deleteOldCaches());
 });
 
+// Function to add resources to the cache
 const addResourcesToCache = async (resources) => {
-const cache = await caches.open(currentVersion);
-await cache.addAll(resources);
+    // Open the cache for the current version
+    const cache = await caches.open(currentVersion);
+    // Add the resources to the cache
+    await cache.addAll(resources);
 };
 
+// Array of resources to precache
 const precache_arr = [
-'/font.css','/icon.css','/index.html',
-'/main.css','/main.js','/main.webmanifest',
-'/register.js'
+    '/font.css','/icon.css','/index.html',
+    '/main.css','/main.js','/main.webmanifest',
+    '/register.js'
 ];
 
+// Event listener for when the service worker is installed
 self.addEventListener('install', (event) => {
-event.waitUntil(addResourcesToCache(precache_arr));
+    // Add the resources to the cache
+    event.waitUntil(addResourcesToCache(precache_arr));
 });
 
+// Function to handle responses for fetch events
 const responseHandle = async (event) => {
-const cachedResponse = await caches.open(currentVersion).then(async (cache) => {
-const cachedResp = await cache.match(event.request);
-return cachedResp;
-});
-const fetchedResponse = fetch(event.request).then(async (networkResponse) => {
-const cache = await caches.open(currentVersion);
-await cache.put(event.request, networkResponse.clone());
-return networkResponse;
-});
-return cachedResponse || fetchedResponse;
+    // Try to get a cached response for the request
+    const cachedResponse = await caches.open(currentVersion).then(async (cache) => {
+        const cachedResp = await cache.match(event.request);
+        return cachedResp;
+    });
+    // If there is no cached response, fetch the request and add it to the cache
+    const fetchedResponse = fetch(event.request).then(async (networkResponse) => {
+        const cache = await caches.open(currentVersion);
+        await cache.put(event.request, networkResponse.clone());
+        return networkResponse;
+    });
+    // Return the cached response or the fetched response
+    return cachedResponse || fetchedResponse;
 }
 
+// Event listener for fetch events
 self.addEventListener('fetch', async (event) => {
-// if (event.request.url.match('^.*(\.mp3)$')) {return false;};
-if (event.request.url.match('^.*(www\.buzzsprout\.com)*.$')) {
-await event.respondWith(fetch(event.request));
-} else {
-await event.respondWith(responseHandle(event));
-};
+    // If the request is for an mp3 file, do not respond with the service worker
+    // if (event.request.url.match('^.*(\.mp3)$')) {return false;};
+    // If the request is for a Buzzsprout URL, fetch it normally
+    if (event.request.url.match('^.*(www\.buzzsprout\.com)*.$')) {
+        await event.respondWith(fetch(event.request));
+    } else {
+        // Otherwise, handle the response with the service worker
+        await event.respondWith(responseHandle(event));
+    };
 });
